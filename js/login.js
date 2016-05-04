563b7b99a81634969e1b15af5e97cd9581c2864e
Dinsio <dinsio@outlook.com>
2016-05-05 00:39:36 +0800

    Add auth method

--- js/login.js
+++ js/login.js
@@ -35,7 +35,7 @@ $(document)
     $('#check-login').on('click',function(){
       if ($('.ui.form').form('is valid'))
       {
-        
+        login($('#email').val(),$('#password').val());
       }
     });
   })
@@ -43,4 +43,34 @@ $(document)
 
 //
 // 建立wilddog链接
-var ref = new Wilddog("https://dinsio-blog.wilddogio.com/");
+var conn = new Wilddog("https://dinsio-blog.wilddogio.com/");
+// 匿名登录函数
+function loginAnonymously(){
+  conn.authAnonymously(
+    function(err,data){
+      if(err == null){
+        console.log("auth success! msg:",data);
+      } else {
+        console.log("auth failed, msg:",err);
+      }
+    }
+  );
+};
+// 自动匿名登录
+loginAnonymously();
+// 博主登录函数
+function login(email,password){
+  // 先注销匿名登录
+  conn.unauth();
+  conn.authWithPassword({email:email,password:password},
+    function(err,data){
+      if(err == null){
+        console.log("auth success! msg:",data);
+      } else {
+        // 重新匿名登录
+        loginAnonymously();
+        console.log("auth failed, msg:",err);
+      }
+    }
+  );
+};
