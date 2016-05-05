4d5b49c8fdc6e18c3dc7072402528e87fc51a33e
Dinsio <dinsio@outlook.com>
2016-05-05 18:48:18 +0800

    add qiniu demo

---
+++ demo/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': 'ZwXrin6ZGzFCYD_npAeD3OHwALCC1GBvIZpMm1RW',
+    'SECRET_KEY': '_9ppUy0WmPoHsAtJpMJD5S02IOBnd-izgTcpwU7D',
+    'Bucket_Name': 'dinsio-blog',
+    'Port': 19110,
+    'Uptoken_Url': 'uptoken',
+    'Domain': 'http://qiniu-plupload.qiniudn.com/'
+};
